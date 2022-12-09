a79dba024696a105fa5b6695b222ee32204fef05
tcarrere <tomascarrere4321@gmail.com>
2022-12-08 23:04:05 -0300

    agrego js

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+//funcion mayor edad del index//
+
+function mostrarMensaje() {
+    var edad = window.confirm('¿Sos mayor de 18 años?');
+        if (edad === true) {
+            window.alert('¡Bienvenido/a a BioSer!');
+        } else { 
+            window.alert('Estas ingresando bajo tu propia responsabilidad');
+        }
+    
+    }
+
+
