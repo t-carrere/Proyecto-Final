ab3045f314aeccaffa934a88aab0254690d25cc1
tcarrere <tomascarrere4321@gmail.com>
2022-12-11 22:05:54 -0300

    agregue contenido

--- javascript/app.js
+++ javascript/app.js
@@ -11,4 +11,43 @@ function mostrarMensaje() {
     }
 
 
+    let facebook = document.querySelector(".facebook");
+facebook.addEventListener("click",()=>{
 
+    let decision =confirm("Vas a salir de la página. ¿Estás seguro?");
+    if (decision == true){
+        location.href= "https://www.facebook.com/bioserokk/"
+        
+    }else{
+        alert("Vas a permanecer en el sitio.");
+    }
+
+
+})
+let instagram = document.querySelector(".instagram");
+instagram.addEventListener("click",()=>{
+
+    let decision =confirm("Vas a salir de la página. ¿Estás seguro?");
+    if (decision == true){
+        location.href= "https://www.instagram.com/bioser_ok/"
+        
+    }else{
+        alert("Vas a permanecer en el sitio.");
+    }
+
+
+})
+
+let whatsapp = document.querySelector(".whatsapp");
+whatsapp.addEventListener("click",()=>{
+
+    let decision =confirm("Vas a salir de la página. ¿Estás seguro?");
+    if (decision == true){
+        location.href= "https://api.whatsapp.com/send?phone=3364620890"
+        
+    }else{
+        alert("Vas a permanecer en el sitio.");
+    }
+
+
+})
